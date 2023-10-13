d1df5da202b9c2658de929139958e8fd3414cbdc
Jhordan Rodnman Moreno Bocanegra <jordanrodman09@gmail.com>
2023-10-13 14:26:41 -0500

    Portafolio terminado

---
+++ Documents/Mi portfolio V2/src/content_option.js
@@ -0,0 +1,134 @@
+const logotext = "Jhordan Rodnman";
+const meta = {
+    title: "Jhordan Rodnman",
+    description: "I’m Jhordan Rodnman Systems Engineer and Full stack developer,currently working in Colombia",
+};
+
+const introdata = {
+    title: "I’m Jhordan Rodnman",
+    animated: {
+        first: "I love creative coding",
+        second: "I code cool and responsive websites",
+        third: "I develop innovation for your company",
+    },
+    description: "Feel free to take a look at my portfolio, and if you wish, we can get in touch with just one click.",
+    your_img_url: "https://i.ibb.co/pxgLQ0m/Me.jpg",
+};
+
+const dataabout = {
+    title: "About me",
+    aboutme: "Systems Engineer, software developer, with more than 4 years of professional experience developing websites, applications and plataforms.",
+};
+const worktimeline = [{
+        jobtitle: "Software Developer",
+        where: "MINTIC, ICESI, INNPULSA",
+        date: "2022 - 2023",
+    },
+    {
+        jobtitle: "Coordinator and Software Developer",
+        where: "Río Software Academy",
+        date: "2021 - present.",
+    },
+    {
+        jobtitle: "Coordinator",
+        where: "Escuela de Robótica del Chocó",
+        date: "2019 - present.",
+    },
+    {
+        jobtitle: "Software Developer",
+        where: "Freelance",
+        date: "2017 - present.",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 80,
+    },
+    {
+        name: "HTML",
+        value: 100,
+    },
+    {
+        name: "CSS",
+        value: 100,
+    },
+    {
+        name: "Javascript",
+        value: 100,
+    },
+    {
+        name: "ReactJS",
+        value: 80,
+    },
+    {
+        name: "C#",
+        value: 80,
+    },
+    {
+        name: "C++",
+        value: 80,
+    }
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Developing user experiences and interactions.",
+    },
+    {
+        title: "Websites",
+        description: "Building websites starting from ideas and dreams.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "The easiest way to convert your ideas into a website.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://i.ibb.co/MNnfgBs/Price-Checker.png",
+        description: "PriceCheker",
+        link: "https://pricechecker.com.co/",
+    },
+    {
+        img: "https://i.ibb.co/FxcC7yM/Promodess.png",
+        description: "Fundación Promotores de Desarrollo Sostenible.",
+        link: "https://promodess.org/",
+    },
+    {
+        img: "https://i.ibb.co/VmNBvMh/Tour-Choc.png",
+        description: "Tour Chocó",
+        link: "https://github.com/jordanrodman/RD",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "jordanrodman09@gmai.com",
+    YOUR_FONE: "(+57)3225195540",
+    description: "Let's talk about software development and programming.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/jordanrodman",
+    facebook: "https://www.facebook.com/JhordanRd/",
+    linkedin: "https://www.linkedin.com/in/jhordanrd/",
+    twitter: "https://twitter.com/jordanrodmanmor",
+    youtube: "https://www.youtube.com/@SuperJordanrodman/"
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
